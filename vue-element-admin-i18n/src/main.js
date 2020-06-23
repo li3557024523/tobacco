f64b5c0fedecbf02b917b4c64d8e4a1b328a5f8b
gdlc778 <3557024523@qq.com>
2020-06-23 16:27:50 +0800

    日志管理

--- vue-element-admin-i18n/src/main.js
+++ vue-element-admin-i18n/src/main.js
@@ -21,7 +21,6 @@ import './permission' // permission control
 import './utils/error-log' // error log
 
 import * as filters from './filters' // global filters
-````````
 /**var ElTreeGrid = require('element-tree-grid');
 Vue.component(ElTreeGrid.name,ElTreeGrid);*/
 /**
